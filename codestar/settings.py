f5dc1673d808f72516a17633cb8176de1df1b74f
Antonyeaster <>
2023-10-31 07:02:37 +0000

    Update allowed hosts for heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-think-blog-antony.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [
+    'django-think-blog-antony-d3d5e08b094c.herokuapp.com', 'localhost']
 
 
 # Application definition
